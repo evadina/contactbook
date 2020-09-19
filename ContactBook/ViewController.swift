9dfdf9cd599b9564bc96d725b31e110ee498906e
Madina <madinatazhiyeva@gmail.com>
2020-09-19 18:46:34 +0600

    Initial Commit

---
+++ ContactBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactBook
+//
+//  Created by Madina Tazhiyeva on 9/19/20.
+//  Copyright © 2020 Madina Tazhiyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
